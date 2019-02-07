36ca82b4c40c03c0b497814c5eca34ea890d2ce4
Christopher C. Wells <chris@chrxs.net>
2019-02-07 11:28:39 -0800

    Refactor PbsAuth to extend new NetworkBase (Social Auth Issue #3030352).

--- src/Plugin/Network/PbsAuthInterface.php
+++ src/Plugin/Network/PbsAuthInterface.php
@@ -2,7 +2,7 @@
 
 namespace Drupal\social_auth_pbs\Plugin\Network;
 
-use Drupal\social_api\Plugin\NetworkInterface;
+use Drupal\social_auth\Plugin\Network\NetworkInterface;
 
 /**
  * Defines the PBS Auth interface.
